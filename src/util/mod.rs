998eabe7abd87c1e4962df753762604820501f68
Steven Fackler <sfackler@gmail.com>
2015-01-18 16:50:52 -0800

    Add a ScheduledThreadPool
    This isn't based off of a DelayQueue because I unfortunately can't think
    of a way to tell the worker threads to shut down when the pool destructs
    *while* making sure they execute all remaining work.

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -3,6 +3,7 @@ pub use self::thread_pool::ThreadPool;
 pub use self::queue::{Queue, SyncQueue};
 pub use self::run::Run;
 pub use self::delay_queue::DelayQueue;
+pub use self::scheduled_thread_pool::ScheduledThreadPool;
 
 pub mod async;
 pub mod atomic;
@@ -11,3 +12,4 @@ mod thread_pool;
 mod queue;
 mod run;
 mod delay_queue;
+mod scheduled_thread_pool;
